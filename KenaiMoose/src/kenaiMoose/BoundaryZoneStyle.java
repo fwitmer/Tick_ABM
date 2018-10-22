a9568ea5aa995d14d4a9b0a899df0bf7dc2fe36c
kristophercarroll <kristophercarroll@kristophers-macbook-pro.local>
2018-10-21 19:23:42 -0800

    Added BoundaryZoneStyle for boundary zone visualization

---
+++ KenaiMoose/src/kenaiMoose/BoundaryZoneStyle.java
@@ -0,0 +1,42 @@
+package kenaiMoose;
+
+import java.awt.Color;
+
+import kenaiMoose.BoundaryZone;
+import gov.nasa.worldwind.render.SurfacePolygon;
+import gov.nasa.worldwind.render.SurfaceShape;
+import repast.simphony.visualization.gis3D.style.SurfaceShapeStyle;
+
+public class BoundaryZoneStyle implements SurfaceShapeStyle<BoundaryZone> {
+
+	@Override
+	public SurfaceShape getSurfaceShape(BoundaryZone object, SurfaceShape shape) {
+		return new SurfacePolygon();
+	}
+
+	@Override
+	public Color getFillColor(BoundaryZone zone) {
+		return Color.YELLOW;
+	}
+
+	@Override
+	public double getFillOpacity(BoundaryZone obj) {
+		return 0.25;
+	}
+
+	@Override
+	public Color getLineColor(BoundaryZone zone) {
+		return Color.BLACK;
+	}
+
+	@Override
+	public double getLineOpacity(BoundaryZone obj) {
+		return 1.0;
+	}
+
+	@Override
+	public double getLineWidth(BoundaryZone obj) {
+		return 3;
+	}
+
+}
