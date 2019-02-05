181197c76866c3954e48ca5456e3b83f44b265af
kristophercarroll <kristophercarroll@10.0.1.62>
2019-02-04 21:56:37 -0900

    Added shell of Tick class
    	- Geometry boundary sensing
    	- Counter for time latched
    	- Moose for associating with Moose host
    	- Needs: implement logic for checking for nearby vectors
    			 implement logic for latching and riding with vector host

---
+++ KenaiMoose/src/kenaiMoose/Tick.java
@@ -0,0 +1,26 @@
+package kenaiMoose;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+
+import repast.simphony.engine.schedule.ScheduleParameters;
+import repast.simphony.engine.schedule.ScheduledMethod;
+
+public class Tick {
+	
+	private String name;
+	private Geometry boundary;
+	private GeometryFactory geoFac = new GeometryFactory();
+	private int latchCount = 0;
+	private Moose ride;
+	
+	@ScheduledMethod(start = 1, interval = 1, priority = ScheduleParameters.FIRST_PRIORITY)
+	public void step() {
+		
+	}
+	// Logic checking for nearby vectors within specified range for latching
+	public void checkForVector() {
+		
+	}
+
+}
