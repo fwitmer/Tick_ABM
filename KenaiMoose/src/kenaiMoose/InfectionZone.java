764049cd88ff1b53ab892d109b6dc766e6c4c2f1
kristophercarroll <kristophercarroll@kristophers-macbook-pro-2.local>
2019-03-24 20:13:03 -0800

    Added InfectionZone agent and InfectionZoneStyle
     - InfectionZone is an agent representing a circular buffer around
       Vector agents
     - Removed GIS 2D Display from simulation GUI
     - Added InfectionZone using InfectionZoneStyle in GIS 3D Display

---
+++ KenaiMoose/src/kenaiMoose/InfectionZone.java
@@ -0,0 +1,16 @@
+package kenaiMoose;
+
+public class InfectionZone {
+	
+	protected boolean visible = true;
+
+	
+	public boolean isVisible() {
+		return visible;
+	}
+	
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+
+}
