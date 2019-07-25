c66f5dc2b0d8a93016ff46de31b5830b69e1b7f9
Hillari <hillari@gmail.com>
2019-07-24 16:59:20 -0800

    Small host movement implemented

--- KenaiMoose/src/kenaiMoose/Vole.java
+++ KenaiMoose/src/kenaiMoose/Vole.java
@@ -1,8 +1,10 @@
 package kenaiMoose;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.Point;
 
@@ -11,38 +13,24 @@ import repast.simphony.gis.util.GeometryUtil;
 import repast.simphony.random.RandomHelper;
 
 public class Vole extends Host {
-	private double vole_range; //? how to determine voles range? What type should this be? Geometry? 
-	//Geometry voleBuffer; //do we need a Geometry object to create buffer?
-	protected InfectionZone smHost_buffer;
-	Geometry vole_home = GeometryUtil.generateBuffer(geography, geography.getGeometry(this), vole_range);
+	private double vole_range;
+	//protected InfectionZone smHost_buffer;
+	Geometry vole_home;
 	
 	
 	
 	public Vole(String name) {
 		super(name); //calling parent constructor 
-		vole_range = 100; 
+		vole_range = 565; // circle with radius 565m = 1.003km^2
 	}
 
 	@Override
 	@ScheduledMethod(start=0) //need to call before first tick in model
 	public void init() {
 		super.init(); 
-		//addBuffer(vole_range); //create buffer (also adds to context)
-		create_home(vole_range); //same as addBuffer method...
+		vole_home = addBuffer(vole_range); //create buffer (also adds to context)
+		//create_home(vole_range); //same as addBuffer method...
 	
-	}
-	
-	protected Geometry create_home(double range) {
-		//FIXME move this to host?
-		//crate a Geometry representing buffer around smHost
-		Geometry smHost_home = GeometryUtil.generateBuffer(geography, geography.getGeometry(this), vole_range);
-		Geometry home_geom = geoFac.createGeometry(smHost_home);
-		
-		smHost_buffer = new InfectionZone();
-		context.add(smHost_buffer);
-		geography.move(smHost_buffer, home_geom);
-		
-		return smHost_home;
 	}
 	
 	@Override
@@ -50,51 +38,43 @@ public class Vole extends Host {
 	public void step() {
 		walk();
 		List<Tick> tickList = getTicks();
-		processInfections(tickList);
+		processInfections(tickList); //TODO change this, will move buffer
 		
 	}
 
 	@Override
+	//Vole needs it's own method here b/c the parent class method moves the geometry and we don't want it to move
+	protected List<Tick> getTicks(){
+		Envelope infection_envelope = vole_home.getEnvelopeInternal();
+		
+		Iterable<Tick> infectingTicks = geography.getObjectsWithin(infection_envelope, IxPacificus.class);
+		List<Tick> tickList = new ArrayList();
+		
+		for (Tick tick : infectingTicks) {
+			if(!tick.isAttached()) {
+				tickList.add((Tick)tick);
+			}
+		}
+		return tickList;
+	}
+	
+	@Override
 	protected void walk() {
-		//do I need prev Coordinate AND prev Point? 
-		//TODO change boundary behavior
-		
+		Coordinate coord;
+		Point pt;
 		//save location in case we need to revert back from an invalid move 
-		Coordinate prev_coord = getCoord();
-		Point prev_point = getPoint();
-		
-		//create new location based off previous (play with these values)
-		Coordinate new_coord = new Coordinate(prev_coord.x += RandomHelper.nextDoubleFromTo(-0.0050, 0.0050), 
-				prev_coord.y += RandomHelper.nextDoubleFromTo(-0.0001, 0.0001));
-		Point newPoint = geoFac.createPoint(new_coord);
-		
-		//check if we are in boundary and not in water 
-		if(!newPoint.within(boundary) || isWater(new_coord)) {
-			System.out.println("Invalid move");
-		}
-		
-		// Create a new point to move to figure out appropriate value for nextDouble
-		new_coord.x = prev_coord.x + RandomHelper.nextDoubleFromTo(-0.0010, 0.0010);
-		new_coord.y = prev_coord.y + RandomHelper.nextDoubleFromTo(-0.0010, 0.0010);
-		newPoint = geoFac.createPoint(new_coord);
+//		Coordinate prev_coord = getCoord();
+//		Point prev_point = getPoint();
 		
 		
-		//FIXME want to somehow get range of buffer so agent can't move outside of it
-		// How to get buffer....return from 
-		if (!newPoint.within(vole_home)) {
-			System.out.println("SmHost tried to move outside of home range");
-			
-		}
 		
-//		//if the new location is within the range, move it...I feel like this isn't right?
-//		if (new_coord.distance(prev_coord) <= vole_range) {
-//			geography.move(this, newPoint);
-//		}
+		do {
+			List<Coordinate> test_coords = GeometryUtil.generateRandomPointsInPolygon(vole_home, 1);
+			coord = test_coords.get(0);
+			pt = geoFac.createPoint(coord); //Create a point based off the coordinate so we can check boundary 
+		} while (isWater(coord) || !pt.within(boundary));
 		
-			//move the agent and update it's buffer
-		geography.move(this, newPoint);	
-		updateInfectionZone();
-		
+		geography.move(this, pt);	//move agent
 	}
 
 }
