91aa467aa63e87ac40c4729586553f51707f2d2a
Kristopher Carroll <kjcarroll@alaska.edu>
2019-07-29 01:11:21 -0800

    Changed attach length to be specific to life stage

--- KenaiMoose/src/kenaiMoose/IxPacificus.java
+++ KenaiMoose/src/kenaiMoose/IxPacificus.java
@@ -10,10 +10,11 @@ public class IxPacificus extends Tick {
 
 	public IxPacificus(String name) {
 		super(name);
-		set_attach_length(7);
 		set_attach_delay(20);
 		EGG_LENGTH = 55;
 		LARVA_LENGTH = 365;
+		LARVA_FEED_LENGTH = 7;
+		set_attach_length(LARVA_FEED_LENGTH);
 		NYMPH_LENGTH = 270;
 		ADULT_LENGTH = 90;
 		
